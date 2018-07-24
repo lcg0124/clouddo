d655df5296ccd9ba5f7eb74eedfb266e59cf4d6a
李春光 <1992lcg@163.com>
2018-07-24 13:41:02 +0800

    异步保存日志

---
+++ clouddo-common/src/main/java/com/bootdo/clouddocommon/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.bootdo.clouddocommon.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
--- clouddo-common/src/main/java/com/bootdo/clouddocommon/service/LogRpcService.java
+++ clouddo-common/src/main/java/com/bootdo/clouddocommon/service/LogRpcService.java
@@ -5,11 +5,13 @@ import com.bootdo.clouddocommon.intercepter.FeignIntercepter;
 import com.bootdo.clouddocommon.utils.R;
 import feign.Headers;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Headers("Content-Type:application/json")
 @FeignClient(name = "api-base", configuration = FeignIntercepter.class)
 public interface LogRpcService {
+    @Async
     @PostMapping("log/save")
     R save(LogDO logDO);
 }
