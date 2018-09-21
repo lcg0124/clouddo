cf2083fe3b9739c63fcfe70d94ca7f7a700f7033
xiaoming <fymhunan@163.com>
2018-09-21 16:38:22 +0800

    解决springboot升级至2.x导致前台登录出现401错误BUG

--- clouddo-admin/src/main/java/com/bootdo/clouddoadmin/ClouddoAdminApplication.java
+++ clouddo-admin/src/main/java/com/bootdo/clouddoadmin/ClouddoAdminApplication.java
@@ -1,7 +1,9 @@
 package com.bootdo.clouddoadmin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = {"com.bootdo.clouddoadmin", "com.bootdo.clouddocommon"})
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class ClouddoAdminApplication {
 
     public static void main(String[] args) {
